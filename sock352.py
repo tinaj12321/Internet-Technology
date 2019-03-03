2022bcbd34091a8d1e0bb83e0f14e9a668023940
Alison Thi <alisothi@gmail.com>
2019-03-03 17:11:04 -0500

    Update sock352.py

--- sock352.py
+++ sock352.py
@@ -1,23 +1,58 @@
 import binascii
 import socket as syssock
+import threading
 import struct
 import sys
+
+# Flags
+SOCK352_SYN = 0x01          # initiate connection
+SOCK352_FIN = 0x02          # end connection
+SOCK352_ACK = 0x04          # ack number
+SOCK352_RESET = 0x08        # reset the connection
+SOECK352_HAS_OPT = 0xA0     # option field is valid
+
+sock352PktHdrData = '!BBBBHHLLQQLL'
 
 # these functions are global to the class and
 # define the UDP ports all messages are sent
 # and received from
 
-def init(UDPportTx,UDPportRx):   # initialize your UDP socket here 
+def init(UDPportTx,UDPportRx):   # initialize your UDP socket here
+	if UDPportRx == 0:
+		UDPportRx = 42069
+	if UDPportTx == 0:
+		UDPportTx = 42069
 	pass 
     
 class socket:
 	def __init__(self):  # fill in your code here 
+		self.socket = syssock.socket(syssock.AF_INET, syssock.SOCK_DGRAM)
         return
 
 def bind(self,address):
         return
 
 def connect(self,address):  # fill in your code here
+	
+	
+	
+	
+        version = 0x1       # Part 1
+        flags = 0           # Part 1
+        opt_ptr = 0
+        protocol = 0
+        header_len = 0
+        checksum = 0
+        source_port = 0
+        dest_port = 0
+        seq_no = 0          # Part 1
+        ack_no = 0          # Part 1
+        window = 0
+        payload_len = 0     # Part 1
+        udpPkt_hdr_data = struct.Struct(sock352PktHdrData)      # returns struct obj, R/W binary data according to the given format
+        header_len = struct.size(udpPkt_hdr_data)
+        header = udpPkt_hdr_data.pack(version, flags, opt_ptr, protocol, checksum,
+                                         source_port, dest_port, seq_no, ack_no, window, payload_len)
 	return
 
 def listen(self,backlog):
@@ -32,7 +67,7 @@ def close(self):   # fill in your code here
 
 def send(self,buffer):
 	bytessent = 0     # fill in your code here
-	return bytesent
+	return bytessent
 
 def recv(self,nbytes):
 	bytesreceived = 0     # fill in your code here
