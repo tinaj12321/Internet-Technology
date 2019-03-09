1e319f65293d1e68f33d8c5060184e798aa13b91
Alison Thi <alisothi@gmail.com>
2019-03-09 17:20:33 -0500

    Update sock352.py

--- sock352.py
+++ sock352.py
@@ -14,6 +14,7 @@ portTx = 0
 portRx = 0
 
 sock352PktHdrData = '!BBBBHHLLQQLL'
+max_packsize = 64000        # in bytes
 # these functions are global to the class and
 # define the UDP ports all messages are sent
 # and received from
@@ -26,7 +27,27 @@ def init(UDPportTx,UDPportRx):   # initialize your UDP socket here
 	portTx = UDPportTx
 	portRx = UDPportRx
 	pass 
-    
+
+def parse_flag(flags):    # process header flags
+    while flags > 0:
+        if flags > 160:     # SOCK352_HAS_OPT
+            opt = 1
+            flags -= 160
+        elif flags > 8:     # SOCK352_RESET = 0x08
+            reset = 1
+            flags -= 8
+        elif flags > 4:     # SOCK352_ACK = 0x04
+            ack = 1
+            flags -= 4
+        elif flags > 2:     # SOCK352_FIN = 0x02
+            fin = 1
+            flags -= 2
+        elif flags > 1:     # SOCK352_SYN = 0x01
+            syn = 1
+            flags -= 1
+    f = (opt, reset, ack, fin, syn)
+    return f
+
 class socket:
 	def __init__(self):  # fill in your code here 
 		self.socket = syssock.socket(syssock.AF_INET, syssock.SOCK_DGRAM)
@@ -79,7 +100,40 @@ class socket:
 		return
 
 	def send(self,buffer):
-		bytessent = 0     # fill in your code here
+		load = struct.unpack(sock352PktHdrData, buffer, 0)
+		
+		version = int(load[0])  # Part 1
+        	flags = int(load[1])  # Part 1
+        	opt_ptr = int(load[2])
+        	protocol = int(load[3])
+        	headerlen = int(load[4])
+        	checksum = int(load[5])
+        	source_port = int(load[6])
+        	dest_port = int(load[7])
+        	seq_no = int(load[8])  # Part 1
+        	ack_no = int(load[9])  # Part 1
+        	window = int(load[10])
+        	payload_len = int(load[11])  # Part 1
+		
+		parse_flag(flags)
+        	# returns f = (opt, reset, ack, fin, syn)
+		
+		
+		bytessent = len(buffer)
+	        # send the size of the file as a 4 byte integer
+        	# to the server, so it knows how much to read
+        	# FRAGMENTSIZE = 8192
+        	# while (bytes_to_send > 0):
+        	#     fragment = fd.read(FRAGMENTSIZE)
+        	#     totalsent = 0
+        	#     # make sure we sent the whole fragment
+        	#     while (totalsent < len(fragment)):
+        	#         sent = s.send(fragment[totalsent:])
+        	#         if (sent == 0):
+        	#             raise RuntimeError("socket broken")
+        	#         totalsent = totalsent + sent
+        	#     bytes_to_send = bytes_to_send - len(fragment)	
+		
 		return bytessent
 
 	def recv(self,nbytes):
